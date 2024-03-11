1341a7c5dc2070cf0021b27a1941247373ae8ca6
Matias Hernandez <matias2k00@gmail.com>
2024-03-11 20:24:38 -0300

    desafio finalizado

--- desafio.js
+++ desafio.js
@@ -13,7 +13,7 @@ const jugadorUno = {
   habilidades: {
     ataque: 70,
     velocidad: 30,
-    vida: 60,
+    vida: 90,
     magia: 120,
   },
   articulos: ['espada', 'escudo', 'varita'],
@@ -39,6 +39,9 @@ const jugadorDos = {
 var contadorPuntosJug1 = 0;
 var contadorPuntosJug2 = 0;
 
+let jugadorUnoHabilidades = jugadorUno.habilidades;
+let jugadorDosHabilidades = jugadorDos.habilidades;
+
 //Ganador:
 var ganador;
 
@@ -49,16 +52,6 @@ var ganador;
 //La indicación ++ luego de la variable contadorPuntosJug suma 1 punto al
 //contador. Según la condición que se cumpla, se irán sumando los puntos.
 
-//EJEMPLO ESTRUCTURA DE COMPARACIÓN
-if (habilidadACompararJugador1 > habilidadACompararJugador2) {
-  contadorPuntosJug1 = contadorPuntosJug1 + 1;
-} else if (condiciónCasoContrario) {
-  contadorPuntosJug2++;
-} else {
-  contadorPuntosJug1++;
-  contadorPuntosJug2++;
-}
-
 // (Escribir debajo el código que te permita generar esta comparación)
 
 //-----------------------------------------------------------------------//
@@ -66,20 +59,53 @@ if (habilidadACompararJugador1 > habilidadACompararJugador2) {
 //COMPARACIÓN POR VELOCIDAD:
 //(Escribir debajo el código que te permita generar esta comparación)
 
+if (jugadorUnoHabilidades.velocidad > jugadorDosHabilidades.velocidad) {
+  contadorPuntosJug1++;
+} else if (jugadorDosHabilidades.velocidad > jugadorUnoHabilidades.velocidad) {
+  contadorPuntosJug2++;
+} else {
+  contadorPuntosJug1++;
+  contadorPuntosJug2++;
+}
+
 //-----------------------------------------------------------------------//
 
 //COMPARACIÓN POR NIVEL DE VIDA:
 //(Escribir debajo el código que te permita generar esta comparación)
+if (jugadorUnoHabilidades.vida > jugadorDosHabilidades.vida) {
+  contadorPuntosJug1++;
+} else if (jugadorDosHabilidades.vida > jugadorDosHabilidades.vida) {
+  contadorPuntosJug2++;
+} else {
+  contadorPuntosJug1++;
+  contadorPuntosJug2++;
+}
 
 //-----------------------------------------------------------------------//
 
 //COMPARACIÓN POR MAGIA:
 //(Escribir debajo el código que te permita generar esta comparación)
+if (jugadorUnoHabilidades.magia > jugadorDosHabilidades.magia) {
+  contadorPuntosJug1++;
+} else if (jugadorDosHabilidades.magia > jugadorDosHabilidades.magia) {
+  contadorPuntosJug2++;
+} else {
+  contadorPuntosJug1++;
+  contadorPuntosJug2++;
+}
 
 //-----------------------------------------------------------------------//
 
 //COMPARACIÓN POR CANTIDAD DE ARTÍCULOS
 //(Escribir debajo el código que te permita generar esta comparación)
+if (jugadorUno.articulos.length > jugadorUno.articulos.length) {
+  contadorPuntosJug1++;
+} else if (jugadorDos.articulos.length > jugadorUno.articulos.length) {
+  contadorPuntosJug2++;
+} else {
+  contadorPuntosJug1++;
+  contadorPuntosJug2++;
+}
 
 //-----------------------------------------------------------------------//
 
@@ -87,11 +113,19 @@ if (habilidadACompararJugador1 > habilidadACompararJugador2) {
 //En este espacio deberás generar la comparación final de puntos
 //que determine al ganador. Mostrar en la terminal el nombre del jugador que ganó la partida
 
+if (contadorPuntosJug1 > contadorPuntosJug2) {
+  ganador = jugadorUno.nombre;
+} else if (contadorPuntosJug2 > contadorPuntosJug1) {
+  ganador = jugadorDos.nombre;
+} else {
+  ganador = 'Hay un empate';
+}
+
 // ejemplo:
-// var resultado = {
-//  [nombreDeJug1]: contadorPuntosJug1,
-//  [nombreDeJug2]: contadorPuntosJug2,
-//  ganador: nombreDeJug1
-// }
-// console.log(resultado)
+var resultado = {
+  [jugadorDos.nombre]: contadorPuntosJug2,
+  [jugadorUno.nombre]: contadorPuntosJug1,
+  ganador: ganador,
+};
+console.log(resultado);
 //-----------------------------------------------------------------------//
