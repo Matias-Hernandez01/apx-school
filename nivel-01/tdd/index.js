dce5814e30bb9eb9db9a3fe97ee4babeb465291c
Matias Hernandez <matias2k00@gmail.com>
2024-04-04 15:09:35 -0300

    homework, funcion cuenta palabra con TDD

--- nivel-01/tdd/index.js
+++ nivel-01/tdd/index.js
@@ -1,5 +1,22 @@
 // Escribir acá la función cuentaPalabras
 
+function cuentaPalabras(texto) {
+  let textoSinEspaciosLaterales = texto.trim().split(' ');
+  let contadorPalabras = textoSinEspaciosLaterales.length;
+  let contadorPalabrasConA = 0;
+
+  textoSinEspaciosLaterales.forEach((palabra) => {
+    if (palabra[0] == 'a') {
+      contadorPalabrasConA += 1;
+    }
+  });
+
+  return {
+    cantidadDePalabras: contadorPalabras,
+    palabrasConA: contadorPalabrasConA,
+  };
+}
+
 // no modificar esta funcion
 function testCuentaPalabras() {
   const texto =
