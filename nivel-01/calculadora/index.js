e6a970a3b1fbf42c3bb93f2b7d0189fc6e695dfe
Matias Hernandez <matias2k00@gmail.com>
2024-04-11 19:02:46 -0300

    Change function of parseArguments

--- nivel-01/calculadora/index.js
+++ nivel-01/calculadora/index.js
@@ -1,6 +1,22 @@
 const process = require('process');
 
+const operaciones = {
+  '+': '+',
+  '-': '-',
+  '/': '/',
+  '*': '*',
+};
+
 function parsearArgumentos(texto) {
+  const separarPorEspacio = texto;
+  let operacion = '';
+  console.log(separarPorEspacio);
+  // separarPorEspacio.forEach((element) => {
+  //   if (element === operacion[element]) {
+  //     operacion = element;
+  //   }
+  // });
+
   return {
     primerTermino: 0,
     segundoTermino: 0,
@@ -10,7 +26,9 @@ function parsearArgumentos(texto) {
 
 function main() {
   const argv = process.argv.slice(2);
-  console.log(argv);
+  // console.log(argv);
+  const resultado = parsearArgumentos(argv);
+  console.log(resultado);
 }
 
 main();
