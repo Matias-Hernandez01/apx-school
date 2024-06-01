5cdb2a89dabab7b15a0ac0d42a8a9f7967fec150
Matias Hernandez <matias2k00@gmail.com>
2024-06-01 11:53:48 -0300

    Change function argumentos and delegarAcciones

--- nivel-01/buscadorDePeliculas/index.js
+++ nivel-01/buscadorDePeliculas/index.js
@@ -6,21 +6,48 @@ function argumentos() {
   let argumento = '';
   let valor = '';
   let argv = process.argv.slice(2, 4);
+  let obj = {};
+
+  //*En caso de recibír argv, se guardará el argumento y el valor.
   for (let palabra of argv) {
     if (palabra.startsWith('--')) {
       argumento = palabra;
     } else {
       valor = palabra;
     }
+    //?Creamos el objeto que va a tener como propiedad el argumento y el valor.
+    obj = {
+      [argumento]: valor,
+    };
   }
-
-  return { argumento: argumento, valor: valor };
+  return obj;
 }
 
-function delegarAcciones(argumento) {}
+function delegarAcciones() {
+  let argumento = '';
+  let valor = '';
+  let argvterminal = argumentos();
+
+  //*Variable que evalua si el comando tiene argumento a traves la longitud del array
+  let condicionArgvVacio = Object.entries(argvterminal);
+
+  //*En caso de que el comando tenga argumento, se guarda en variables para delegar a sus respectivas funciones
+  if (condicionArgvVacio.length === 1) {
+    argumento = condicionArgvVacio[0][0];
+    valor = condicionArgvVacio[0][1];
+  }
+
+  //*En caso de recibir el argv vacio, se retorna todas las peliculas
+  if (!condicionArgvVacio) {
+    return funciones.allMovies();
+    //*Si el argumento ingresado es sort, se envia el valor a su funcion que es encargada de realizar el orden dependiendo su valor.
+  } else if (argumento === '--sort') {
+    return funciones.sort(valor);
+  }
+}
 
 function main() {
-  console.log(argumentos());
+  return delegarAcciones();
 }
 
 main();
