fa36224c777381f8d37ca59ce8dbe212538654d3
Matias Hernandez <matias2k00@gmail.com>
2024-08-16 19:40:43 -0300

    repaso de clases

---
+++ nivel-01/POO/ClasesConClases/index.ts
@@ -0,0 +1,26 @@
+class Album {
+  title: string;
+  songs: string[];
+
+  constructor(title: string, songs: string[]) {
+    this.title = title;
+    this.songs = songs;
+  }
+}
+
+class Banda {
+  members: string[];
+  albums: Album[];
+
+  constructor(members: string[], albums: Album[]) {
+    this.members = members;
+    this.albums = albums;
+  }
+}
+
+function main() {
+  const banda = new Banda(['Pato', 'Maxi'], [{ title: 'Rehen', songs: [] }]);
+  console.log(banda);
+}
+
+main();
