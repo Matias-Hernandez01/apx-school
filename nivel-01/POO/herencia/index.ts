69bad02209ff7df581f5854f3b03ce53e1face33
Matias Hernandez <matias2k00@gmail.com>
2024-08-24 19:57:29 -0300

    tarea finalizada

--- nivel-01/POO/herencia/index.ts
+++ nivel-01/POO/herencia/index.ts
@@ -55,6 +55,26 @@ class ProductoAlimenticio extends Producto {
   }
 }
 
+class ProductoCongelado extends ProductoAlimenticio {
+  temperaturaRecomendada: number;
+  private margen: number;
+  constructor(name, price, fechaCaducidad, temperaturaRecomendada: number, margen: number = 1) {
+    super(name, price, fechaCaducidad);
+    this.temperaturaRecomendada = temperaturaRecomendada;
+    this.margen = margen;
+  }
+
+  isProperlyStored(temperaturaActual: number): boolean {
+    const temperaturaRecomendada = this.temperaturaRecomendada;
+    const margen = this.margen;
+    if (temperaturaActual <= temperaturaRecomendada + margen) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
 function main() {
   // const producto1 = new Producto('Heladera', 400500);
   const productoElectronico = new ProductoElectrónico('Televisor', 300000, true, []);
@@ -63,6 +83,26 @@ function main() {
   const fechaDeHarina = new Date('2024-06-27');
   const harina = new ProductoAlimenticio('Harina', 1500, '2024-06-27');
   console.log(harina.checkCaducidad());
+
+  // Creación de la instancia
+  const helado = new ProductoCongelado('Helado de Vainilla', 2.99, '2024-12-31', -18);
+
+  // Temperatura actual de almacenamiento
+  const temperaturaActual = -6; // Supongamos que esta es la temperatura actual
+
+  // Verificar si el producto está almacenado correctamente
+  const estaAlmacenadoCorrectamente = helado.isProperlyStored(temperaturaActual);
+  console.log('¿Está almacenado correctamente?:', estaAlmacenadoCorrectamente ? 'Sí' : 'No');
+
+  // Verificar si el producto está caducado
+  const esCaducado = helado.checkCaducidad();
+  console.log('¿El producto está caducado?:', esCaducado ? 'Sí' : 'No');
+
+  // Mostrar detalles del producto
+  console.log('Nombre del Producto:', helado.name);
+  console.log('Precio:', helado.getPrice());
+  console.log('Fecha de Caducidad:', helado.fechaCaducidad);
+  console.log('Temperatura Recomendada:', helado.temperaturaRecomendada, 'grados');
 }
 
 main();
